b80eecd8a6b4cb8242246b7110be3c8268efe47a
Thomas Schultz <tjschultz@mwc-039124.dhcp.missouri.edu>
2018-02-01 21:20:20 -0600

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Thomas Schultz on 2/1/18.
+//  Copyright © 2018 Thomas Schultz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
