8a7be26787eac02003a198274c12ead8ec480a29
Thomas Schultz <tjschultz@mwc-039124.dhcp.missouri.edu>
2018-02-01 21:54:11 -0600

    Created control flow statements to print values

--- Swift Language Basics/main.swift
+++ Swift Language Basics/main.swift
@@ -22,3 +22,77 @@ let integral: Character = "\u{222B}"
 let greeting: String = "Hello"
 var name: String = "Karen"
 
+if sample1 == sample2
+{
+    print("The samples are equal.")
+}
+else
+{
+    print("The samples are not equal.")
+}
+
+
+if heartRate >= 40 && heartRate =< 80
+{
+    print("Heart rate is normal.")
+}
+else
+{
+    print( "Heart rate is not normal.")
+}
+
+
+if deposits >= 100000000
+{
+    print("You are exceedingly wealthy.")
+}
+else
+{
+    print("Sorry you are so poor.")
+}
+
+
+var force: Float = mass * acceleration
+print("Force =", force)
+print(distance, "is the distance")
+
+
+if lost == true && expensive == true
+{
+    print("I am really sorry! I will get the manager.")
+}
+else if lost == true && expensive == false
+{
+    print("Here is coupon for 10% off.")
+}
+
+
+switch choice
+{
+case 1:
+    print("You chose 1.")
+case 2:
+    print("You chose 2.")
+case 3:
+    print("you chose 3.")
+default:
+    print("You made an unknown choice.")
+}
+
+print(integral, " is an integral.")
+var i = 5
+for i in 5...10
+{
+    print("i = \(i)")
+}
+
+
+var age: Int = 0
+while age < 6
+{
+    print("age = \(age)")
+    age += 1
+}
+
+print("\(greeting) \(name)")
+
